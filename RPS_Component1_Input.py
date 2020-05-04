e01cfce70ad4f8e4b7104b9cd76266457e0c3cc9
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-05-04 16:23:18 +1200

    Had to do some alteration

--- RPS_Component1_Input.py
+++ RPS_Component1_Input.py
@@ -7,58 +7,59 @@
 
 # Functions go here
 def int_checker(question):
+    while True:
+        try:
+            response = int(input(question))
+            return response
+        except ValueError:
+            print("Please enter a whole number above 0")
 
-    try:
-        response = int(input(question))
-    except ValueError:
-        print("Please enter a whole number")
-    return response
 
 # Main code
-
 game_mode_valid = False
 while not game_mode_valid:
     # Ask user which game mode they would like to play
     game_mode = input("Normal(N) or Variant(V)?").lower()
 
     # Give user confirmation on which game mode they have chosen
-    if game_mode == "n" or "normal":
+    if game_mode == "n" or game_mode == "normal":
         print("You have chosen normal rules for rock paper scissors")
         game_mode_valid = True
-    elif game_mode == "v" or "variant":
-        print("You have chosen normal rules for rock paper scissors")
+    elif game_mode == "v" or game_mode == "variant":
+        print("You have chosen variant rules for rock paper scissors lizard spock")
         game_mode_valid = True
     # If user inputs anything other than the given choices print an error message
     else:
         print("Please choose either Normal(N) or Variant(V) rules")
 
+
 attack_choice_valid = False
 while not attack_choice_valid:
-    if game_mode == "n" or "normal":
+    if game_mode == "n" or game_mode == "normal":
         attack_choice = input("Please chose from Rock(R), Paper(P) or Scissors(S)").lower()
-    elif game_mode == "v" or "variant":
+    elif game_mode == "v" or game_mode == "variant":
         attack_choice = input("Please chose from Rock(R), Paper(P), Scissors(S), Lizard(L) or Spock(Sp)").lower()
 
-    if attack_choice == "r" or "rock":
+    if attack_choice == "r" or attack_choice == "rock":
         chosen_attack = "rock"
-    elif attack_choice == "p" or "paper":
+    elif attack_choice == "p" or attack_choice == "paper":
         chosen_attack = "paper"
-    elif attack_choice == "s" or "scissors":
+    elif attack_choice == "s" or attack_choice == "scissors":
         chosen_attack = "scissors"
-    elif game_mode == "v" or "variant":
-        if attack_choice == "l" or "lizard":
+    elif game_mode == "v" or game_mode == "variant":
+        if attack_choice == "l" or attack_choice == "lizard":
             chosen_attack = "lizard"
-        elif attack_choice == "sp" or "spock":
+        elif attack_choice == "sp" or attack_choice == "spock":
             chosen_attack = "spock"
     else:
         continue
-    print(attack_choice)
+    print(chosen_attack)
     attack_choice_valid = True
 
 rounds_valid = False
 while not rounds_valid:
     rounds = int_checker("Best out of ?:")
     if rounds % 2 == 0:
-        print("Please enter an odd number")
+        print("Please enter an odd number above 0")
         continue
     rounds_valid = True
---
+++ RPS_Component2_CompAttackChoice.py
@@ -0,0 +1,4 @@
+# Rock paper scissors component 2 - Computer Attack Choice Generator
+
+# To Do
+# - Depending on what game_mode the user has chosen generate a choice.
